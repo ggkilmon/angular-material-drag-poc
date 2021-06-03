ecd027fd7d0b8812a6982e87c7745b697131adb8
Graham Kilmon <ggkilmon@gmail.com>
2021-06-02 21:55:23 -0400

    init; structure ui; add drag lists

---
+++ angular-material-drag-poc/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'angular-material-drag-poc';
+
+  items = [1,2,3,4,5,6,7,8,9,10];
+  dests = [11, 12, 13];
+
+  drop(event: CdkDragDrop<string[]>){
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+}
